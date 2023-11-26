f7ae73868085b0303e9b6da913a650e4a7e5af7f
ezriharmusial <ezriharmusial@gmail.com>
2023-11-26 18:29:29 +0100

    Default Images & Url

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Ekesolar.com - ';
-export const SITE_DESCRIPTION = '... description here ...';
+export const SITE_URL = "https://ekesolar.com";
+export const SITE_TITLE_POSTFIX = "- Ekesolar.com";
+export const SITE_TITLE = "Join the _Solar Electrification of Africa";
+export const SITE_DESCRIPTION = "EkeSolar is the initiative of ArtKids Intl. Corp. Holland which focuses on solving Africa's Electricity challenges.";
